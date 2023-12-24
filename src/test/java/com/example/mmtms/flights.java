3177bd6aeae70defcbd314af5d45a6bc185f6200
abhijeet malunjkar <abhijeetmalunkar.sits.it@gmail.com>
2023-12-24 09:38:53 -0800

    code updated

---
+++ src/test/java/com/example/mmtms/flights.java
@@ -0,0 +1,8 @@
+package com.example.mmtms;
+
+public class flights {
+    public static void main( String[] args )
+   {
+               System.out.println( "Welcome to Java Programming!" );
+            }
+}
